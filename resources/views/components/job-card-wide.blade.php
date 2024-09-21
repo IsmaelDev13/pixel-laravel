120fa08314a8da1ba3bea18c95fcd797ee02428d
IsmaelDev13 <ismael-diaz8@hotmail.es>
2024-09-21 20:51:17 +0200

    refactoring compoennts

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,17 @@
+<x-panel class="flex gap-x-6">
+    <div>
+
+       <x-employer-logo/>
+    </div>
+    <div class="flex-1 flex flex-col">
+
+        <a href="#" class="self-start text-sm text-gray-500">Ismael</a>
+            <h3 class="font-bold text-xl mt-3 group-hover:text-blue-700 transition-colors duration-300">Video Producer</h3>
+            <div class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</div>
+    </div>        
+                <div>
+                   <x-tag>Tag</x-tag>
+                   <x-tag>Tag</x-tag>
+                   <x-tag>Tag</x-tag>
+                </div>
+        </x-panel>
