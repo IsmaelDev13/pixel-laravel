2c7d9848d5c1422e6a95275fb51c3b1d1e73cb89
IsmaelDev13 <ismael-diaz8@hotmail.es>
2024-09-22 12:04:36 +0200

    job controller

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,12 @@
+<x>
+  <x-page-heading >Login</x-page-heading>
+  <x-forms.form method="POST" action="/login" enctype="mutipart/form-data">
+
+  <x-forms.input label="Email" type="email" name="email"></x-forms.input>
+  <x-forms.input label="Password" type="password" name="password"></x-forms.input>
+  <x-forms.divider/>
+<x-forms.button>
+  Log In
+</x-forms.button>
+  </x-forms.form>
+</x>
